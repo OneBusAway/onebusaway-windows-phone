f0f1f97c41deaa19ec90398940d812c0de300115
Sinorm_cp <3ace82b990f7b1a93b292ece7e91c2e077325c009J27NPKH>
2010-09-27 07:01:56 +0000

    Merge change from mwfriedm branch -Move the ViewState to the Model
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/onebusawaywp7/OneBusAway;C61565

---
+++ OneBusAway.WP7.ViewModel/ViewState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OneBusAway.WP7.ViewModel.BusServiceDataStructures;
+
+namespace OneBusAway.WP7.ViewModel
+{
+    public class ViewState
+    {
+        private ViewState() { }
+        public static readonly ViewState Instance = new ViewState();
+        public Stop CurrentStop;
+        public Route CurrentRoute;
+        public RouteStops CurrentRouteDirection;
+        public DateTime CurrentTime;
+    }
+}
