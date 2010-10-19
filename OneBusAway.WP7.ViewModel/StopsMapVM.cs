8278fd94efd79ebc77c5309b347de7c3331d2656
Sinorm_cp <3ace82b990f7b1a93b292ece7e91c2e077325c009J27NPKH>
2010-10-19 07:14:21 +0000

    -Clicking on the map in the stops pivot opens a full screen map of stops -Selecting any stop will take you to the details page for that stop
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/onebusawaywp7/OneBusAway;C62421

---
+++ OneBusAway.WP7.ViewModel/StopsMapVM.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using OneBusAway.WP7.ViewModel.BusServiceDataStructures;
+using System.Device.Location;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace OneBusAway.WP7.ViewModel
+{
+    public class StopsMapVM : AViewModel
+    {
+        private Object stopsForLocationLock;
+
+        #region Constructors
+
+        public StopsMapVM()
+            : base()
+        {
+            Initialize();
+        }
+
+        public StopsMapVM(IBusServiceModel busServiceModel, IAppDataModel appDataModel)
+            : base(busServiceModel, appDataModel)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            stopsForLocationLock = new Object();
+            StopsForLocation = new ObservableCollection<Stop>();
+        }
+
+        #endregion
+
+        #region Public Methods/Properties
+
+        public ObservableCollection<Stop> StopsForLocation { get; private set; }
+
+        public void LoadStopsForLocation(GeoCoordinate topLeft, GeoCoordinate bottomRight)
+        {
+            GeoCoordinate center = new GeoCoordinate()
+            {
+                Latitude = (topLeft.Latitude + bottomRight.Latitude) / 2,
+                Longitude = (topLeft.Longitude + bottomRight.Longitude) / 2
+            };
+
+            int radiusInMeters = ((int)topLeft.GetDistanceTo(bottomRight)) / 2;
+            // Query for at least a 250m radius and less than a 1km radius
+            radiusInMeters = Math.Max(radiusInMeters, 250);
+            radiusInMeters = Math.Min(radiusInMeters, 1000);
+
+            operationTracker.WaitForOperation("StopsForLocation");
+            busServiceModel.StopsForLocation(center, radiusInMeters);
+        }
+
+        public override void RegisterEventHandlers()
+        {
+            base.RegisterEventHandlers();
+
+            this.busServiceModel.StopsForLocation_Completed += new EventHandler<EventArgs.StopsForLocationEventArgs>(busServiceModel_StopsForLocation_Completed);
+        }
+
+        public override void UnregisterEventHandlers()
+        {
+            base.UnregisterEventHandlers();
+
+            this.busServiceModel.StopsForLocation_Completed -= new EventHandler<EventArgs.StopsForLocationEventArgs>(busServiceModel_StopsForLocation_Completed);
+
+            // Reset loading to 0 since event handlers have been unregistered
+            this.operationTracker.ClearOperations();
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        void busServiceModel_StopsForLocation_Completed(object sender, EventArgs.StopsForLocationEventArgs e)
+        {
+            Debug.Assert(e.error == null);
+
+            if (e.error == null)
+            {
+                lock (stopsForLocationLock)
+                {
+                    // TODO: This algorithm is pretty slow, around ~1/4 second under debugger
+                    // We should try to find a more efficient way to do this
+                    List<Stop> stopsToRemove = new List<Stop>();
+                    foreach (Stop stop in StopsForLocation)
+                    {
+                        if (e.stops.Contains(stop) == false)
+                        {
+                            stopsToRemove.Add(stop);
+                        }
+                    }
+                    foreach (Stop stop in stopsToRemove)
+                    {
+                        StopsForLocation.Remove(stop);
+                    }
+
+                    foreach (Stop stop in e.stops)
+                    {
+                        if (StopsForLocation.Contains(stop) == false)
+                        {
+                            StopsForLocation.Add(stop);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ErrorOccured(this, e.error);
+            }
+
+            operationTracker.DoneWithOperation("StopsForLocation");
+        }
+
+        #endregion
+
+    }
+}
---
+++ OneBusAway/StopsMapPage.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using OneBusAway.WP7.ViewModel;
+using System.Device.Location;
+using OneBusAway.WP7.ViewModel.BusServiceDataStructures;
+using Microsoft.Phone.Controls.Maps;
+
+namespace OneBusAway.WP7.View
+{
+    /// <summary>
+    /// A full screen map of stops.
+    /// </summary>
+    /// <remarks>
+    /// Supports user interaction.  Will reload stops when moved.  Touch a stop to bring up its detail page.
+    /// </remarks>
+    public partial class StopsMapPage : AViewPage
+    {
+        private StopsMapVM viewModel;
+        private LocationRect previousMapView;
+        private Object mapHasMovedLock;
+        private bool mapHasMoved;
+
+        public StopsMapPage()
+            : base()
+        {
+            InitializeComponent();
+            base.Initialize();
+
+            viewModel = aViewModel as StopsMapVM;
+            previousMapView = DetailsMap.BoundingRectangle;
+            mapHasMoved = false;
+            mapHasMovedLock = new Object();
+            this.Loaded += new RoutedEventHandler(FullScreenMapPage_Loaded);
+
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+            this.DetailsMap.MapZoom += new EventHandler<MapZoomEventArgs>(DetailsMap_MapZoom);
+            this.DetailsMap.MapPan += new EventHandler<MapDragEventArgs>(DetailsMap_MapPan);
+            this.DetailsMap.MouseLeftButtonUp += new MouseButtonEventHandler(DetailsMap_MouseLeftButtonUp);
+
+            SupportedOrientations = SupportedPageOrientation.Portrait;
+        }
+
+        // This method will kick off the initial load of bus stops, and
+        // then unregister itself
+        void DetailsMap_MapResolved(object sender, EventArgs e)
+        {
+            this.DetailsMap.MapResolved -= new EventHandler(DetailsMap_MapResolved);
+            DetailsMap_MouseLeftButtonUp(this, null);
+        }
+
+        void FullScreenMapPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel.LocationTracker.RunWhenLocationKnown(delegate(GeoCoordinate location)
+                {
+                    Dispatcher.BeginInvoke(() =>
+                        {
+                            // If the user has already moved the map, don't relocate it
+                            if (mapHasMoved == false)
+                            {
+                                // Now that we know where we are register the MapResolved event to load
+                                // the initial stops
+                                mapHasMoved = true;
+                                this.DetailsMap.MapResolved += new EventHandler(DetailsMap_MapResolved);
+                                DetailsMap.Center = location;
+                            }
+                        }
+                        );
+                }
+            );
+        }
+
+        void DetailsMap_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+                lock (mapHasMovedLock)
+                {
+                    if (mapHasMoved == false)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        mapHasMoved = false;
+                    }
+                }
+
+                if (BusStopsLayer.Visibility == Visibility.Visible)
+                {
+                    if (LocationRectContainedBy(previousMapView, DetailsMap.BoundingRectangle) == false)
+                    {
+                        viewModel.LoadStopsForLocation(
+                            new GeoCoordinate() { Latitude = DetailsMap.BoundingRectangle.North, Longitude = DetailsMap.BoundingRectangle.West },
+                            new GeoCoordinate() { Latitude = DetailsMap.BoundingRectangle.South, Longitude = DetailsMap.BoundingRectangle.East }
+                            );
+                    }
+
+                    previousMapView = DetailsMap.BoundingRectangle;
+                }
+        }
+
+        void DetailsMap_MapPan(object sender, MapDragEventArgs e)
+        {
+            lock (mapHasMovedLock)
+            {
+                mapHasMoved = true;
+            }
+        }
+
+        void DetailsMap_MapZoom(object sender, MapZoomEventArgs e)
+        {
+            if (DetailsMap.ZoomLevel < 16)
+            {
+                BusStopsLayer.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BusStopsLayer.Visibility = Visibility.Visible;
+                lock (mapHasMovedLock)
+                {
+                    mapHasMoved = true;
+                }
+            }
+        }
+
+        private bool LocationRectContainedBy(LocationRect outer, LocationRect inner)
+        {
+            // TODO: This algorithm will almost certainly fail around the equator
+            if (Math.Abs(inner.North) < Math.Abs(outer.North) && 
+                Math.Abs(inner.South) > Math.Abs(outer.South) &&
+                Math.Abs(inner.West) < Math.Abs(outer.West) && 
+                Math.Abs(inner.East) > Math.Abs(outer.East))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel.RegisterEventHandlers();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            viewModel.UnregisterEventHandlers();
+        }
+
+        private void BusStopPushpin_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedStopId = ((Button)sender).Tag as string;
+
+            foreach (object item in StopsMapItemsControl.Items)
+            {
+                Stop stop = item as Stop;
+                if (stop != null && stop.id == selectedStopId)
+                {
+                    viewModel.CurrentViewState.CurrentStop = stop;
+                    viewModel.CurrentViewState.CurrentRoute = null;
+                    viewModel.CurrentViewState.CurrentRouteDirection = null;
+
+                    NavigationService.Navigate(new Uri("/DetailsPage.xaml", UriKind.Relative));
+
+                    break;
+                }
+            }
+        }
+    }
+}
