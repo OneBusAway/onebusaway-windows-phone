dbdddc9698872788a7f952c1375c2303267c9a88
mwfriedm_cp <15eff1a7ae51e99e8d3f37a919ecb209f7cd5e65xXnLaeRS>
2010-12-01 18:48:17 +0000

    Theme support.  Allows you to toggle between the OBA color theme and the system theme.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/onebusawaywp7/OneBusAway;C64637

--- OneBusAway/DetailsPage.xaml.cs
+++ OneBusAway/DetailsPage.xaml.cs
@@ -79,6 +79,19 @@ namespace OneBusAway.WP7.View
             busArrivalUpdateTimer.Interval = new TimeSpan(0, 0, 0, 30, 0); // 30 secs 
             busArrivalUpdateTimer.Tick += new EventHandler(busArrivalUpdateTimer_Tick);
 
+            this.ApplicationBar.ForegroundColor = ((SolidColorBrush)Application.Current.Resources["OBAForegroundBrush"]).Color;
+            Color obaDarkColor = ((SolidColorBrush)Application.Current.Resources["OBADarkBrush"]).Color;
+            // the native theme uses a shade of "gray" that is actually white or black with an alpha mask.
+            // the appbar needs to be opaque.
+            double alpha = ((double)obaDarkColor.A)/255.0;
+            double backgroundOpacity = (Double)Application.Current.Resources["PhoneLightThemeOpacity"];
+            double compositedBackground = (1 - alpha) * backgroundOpacity * 0xFF;
+            obaDarkColor = Color.FromArgb( 0xFF,
+                (byte)(alpha * obaDarkColor.R + compositedBackground),
+                (byte)(alpha * obaDarkColor.G + compositedBackground),
+                (byte)(alpha * obaDarkColor.B + compositedBackground));
+            this.ApplicationBar.BackgroundColor = obaDarkColor;
+
 #if SCREENSHOT
             SystemTray.IsVisible = false;
 #endif
