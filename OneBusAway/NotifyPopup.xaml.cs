cf7b329a281fef60429ec4f82a77b6513d53bf17
sphenry_cp <a4fa30cb2b6e0722564455522baf61db2ce3a37aKqGV3u4m>
2011-02-25 15:36:48 +0000

    Added Push Notification Popup on press and hold of details
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/onebusawaywp7/OneBusAway;C69402

---
+++ OneBusAway/NotifyPopup.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace OneBusAway.WP7.View
+{
+    public partial class NotifyPopup : UserControl
+    {
+        #region Events
+
+        public event EventHandler<NotifyEventArgs> Notify_Completed;
+        #endregion
+
+        public NotifyPopup()
+        {
+            InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(NotifyPopup_Loaded);
+        }
+
+        void NotifyPopup_Loaded(object sender, RoutedEventArgs e)
+        {
+            SwivelForwardIn.Begin();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dismiss();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (Notify_Completed != null)
+            {
+                int numMinutes = TimePicker.SelectedIndex * 5 + 5;
+                Notify_Completed(this, new NotifyEventArgs(null, numMinutes));
+            }
+
+            Dismiss();
+        }
+
+        private void Dismiss()
+        {
+            SwivelForwardIn.Completed += new EventHandler(SwivelForwardIn_Completed);
+            SwivelForwardOut.Begin();
+        }
+
+        void SwivelForwardIn_Completed(object sender, EventArgs e)
+        {
+        }
+
+    }
+
+    public class NotifyEventArgs : System.EventArgs
+    {
+        public int minutes { get; private set; }
+
+        public NotifyEventArgs(Exception error, int minutes)
+        {
+            this.minutes = minutes;
+        }
+    }
+}
