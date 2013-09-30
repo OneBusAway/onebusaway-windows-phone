f6dadbd1ea0a750fb0e75c3c608226ff6e7ffc10
barbeau_cp <3265ffe114ce3c52095efeec947bb27c399e633fVEuUetme>
2013-09-30 15:11:18 +0000

    Changing the license to use the Apache 2.0 license and updating headers.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/onebusawaywp7/OneBusAway;C103561

--- OneBusAway/TiltEffect.cs
+++ OneBusAway/TiltEffect.cs
@@ -1,12 +1,17 @@
-﻿/* 
-    Copyright (c) 2010 Microsoft Corporation.  All rights reserved.
-    Use of this sample source code is subject to the terms of the Microsoft license 
-    agreement under which you licensed this sample source code and is provided AS-IS.
-    If you did not accept the terms of the license agreement, you are not authorized 
-    to use this sample source code.  For the terms of the license, please see the 
-    license agreement between you and Microsoft.
-*/
-
+﻿/* Copyright 2013 Shawn Henry, Rob Smith, and Michael Friedman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 using System;
 using System.Windows;
